814aef5303754de23a4bd98db09362ca19adb33f
Ashrab Risal <arisal@uncc.edu>
2025-05-09 18:34:46 -0400

    more changes

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary,
     params: {
-        folder: 'images',
+        folder: 'battlebinder',
         allowed_formats: ['jpg', 'png', 'jpeg']
     }
 });
