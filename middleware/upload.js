69a53abbc850f35a61341a32adf95e131ba27eb3
Ashrab Risal <arisal@uncc.edu>
2025-05-09 18:26:38 -0400

    added cloud storage for images

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,17 +1,9 @@
+require('dotenv').config();
 const multer = require('multer');
+const {storage} = require('../middleware/cloudinary');
 const path = require('path');
 const {validateId, validateItem, validateResult} = require('../middleware/validator.js');
 
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './public/images')
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, uniqueSuffix + path.extname(file.originalname))
-    }
-})
 
 exports.upload = multer({ storage, 
                         limits:{fileSize: 2*1024*1024}
